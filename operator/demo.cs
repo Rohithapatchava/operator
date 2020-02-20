c4878d48b1fb4cc5506aa1c8ed3743f45f3c151b
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:24:26 +0530

    Initial commit

---
+++ operator/demo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator
+{
+    struct demo
+{
+    int no;
+    int n1;
+
+
+};
+
+public void showdata()
+{
+    Console.WriteLine("{0}\t{1}", n0, n1);
+}
+public static Demo operator +(demo x, demo y)
+{
+    demo t = new demo();
+    t.no = x.no - y.no;
+    t.no1 = x.no1 - y.n01;
+    return t;
+}
+public static Demo operator -(demo x,demo y)
+{
+    demo t = new demo();
+    t.no = x.no - y.no;
+    t.no1 = x.no1 - y.n01;
+    return t;
+}
+public static Demo operator *(demo x, demo y)
+{
+    demo t = new demo();
+    t.no = x.no * y.no;
+    t.no1 = x.no1 * y.n01;
+    return t;
+}
+public static Demo operator /(demo x, demo y)
+{
+    demo t = new demo();
+    t.no = x.no / y.no;
+    t.no1 = x.no1 / y.n01;
+    return t;
+}
+
+}
+
+
+
